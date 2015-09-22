c3aa7d9eec5d1446242c80622709ac20a8e49af1
NickMattmiller <njmatt03@louisville.edu>
2015-09-21 20:33:09 -0400

    Paper Rock Scissors
    first commit

---
+++ app/src/main/java/com/nickmattmiller/rockpaperscissors/MainActivity.java
@@ -0,0 +1,52 @@
+package com.nickmattmiller.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView rockImage = (ImageView)findViewById(R.id.btnRock);
+
+        rockImage.SetOnClickListner
+        ImageView paperImage = (ImageView)findViewById(R.id.btnPaper);
+
+        ImageView scissorsImage = (ImageView)findViewById(R.id.btnScissors);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
